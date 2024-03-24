ee59c08db892aa607477852df303ae82fd575526
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-24 15:53:12 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -7,6 +7,6 @@ sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/03Aziz02/wordpress.git
+git clone https://github.com/mujahidshaik28/wordpress.git
 cd wordpress
 docker-compose up -d
